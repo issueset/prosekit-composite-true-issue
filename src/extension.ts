10541dc87c28e85b438bebcda263c6192560a689
ocavue <ocavue@gmail.com>
2024-07-24 14:05:32 +1000

    w

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
-import { defineBasicExtension } from 'prosekit/basic'
-import { union } from 'prosekit/core'
-import { defineCodeBlock } from 'prosekit/extensions/code-block'
+import { defineBasicExtension } from "prosekit/basic";
+import { union } from "prosekit/core";
+import { defineCodeBlock } from "prosekit/extensions/code-block";
 
 export function defineExtension() {
-  return union([defineBasicExtension(), defineCodeBlock()])
+  return union([defineBasicExtension(), defineCodeBlock()]);
 }
 
-export type EditorExtension = ReturnType<typeof defineExtension>
+export type EditorExtension = ReturnType<typeof defineExtension>;
